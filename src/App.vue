1a5ee0b6a405a2516c6b8c86b58485e26563fe7c
Jhonnatan Castro <jjhoncv@gmail.com>
2023-09-02 22:38:45 -0500

    test router

--- src/App.vue
+++ src/App.vue
@@ -11,8 +11,8 @@ import HelloWorld from './components/HelloWorld.vue'
       <HelloWorld msg="You did it!" />
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/">Login</RouterLink>
+        <RouterLink to="/about">Empleados</RouterLink>
       </nav>
     </div>
   </header>
